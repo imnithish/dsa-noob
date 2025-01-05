6ffb0001aba41e155c82be277367c1f1ac807b71
imnithish <imnithish@live.com>
2025-01-06 02:18:13 +0530

    Add removeNthFromEnd

--- src/code/Runner.kt
+++ src/code/Runner.kt
@@ -1,8 +1,9 @@
 import code.linkedlist.problems.findKthNodeFromEnd
 import code.linkedlist.problems.hasLoop
 import code.linkedlist.problems.linkedListMiddleNode
+import code.linkedlist.problems.removeNthFromEnd
 
 fun main() {
-    findKthNodeFromEnd()
+    removeNthFromEnd()
 
 }
--- src/code/linkedlist/problems/RemoveNthNodeFromEndOfList.kt
+++ src/code/linkedlist/problems/RemoveNthNodeFromEndOfList.kt
@@ -1,4 +1,42 @@
 package code.linkedlist.problems
 
-class RemoveNthNodeFromEndofList {
+import code.linkedlist.LLNode
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ */
+fun removeNthFromEnd(n: Int = 2) {
+
+    val head = LLNode(1)
+    head.next = LLNode(2)
+    head.next?.next = LLNode(3)
+    head.next?.next?.next = LLNode(4)
+    head.next?.next?.next?.next = LLNode(5)
+
+
+    // Solution starts here
+    val dummy = LLNode(0)
+    dummy.next = head
+
+    var slow: LLNode<Int>? = dummy
+    var fast: LLNode<Int>? = dummy
+
+    for (i in 0..n) {
+        fast = fast?.next
+    }
+
+    while (fast != null) {
+        slow = slow?.next
+        fast = fast.next
+    }
+
+    // remove node
+    val temp = slow?.next
+    slow?.next = temp?.next
+    temp?.next = null
+
+    println(dummy.next)
 }
